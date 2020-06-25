c1fab02ad437f8aa56ccc70bbfd7e5b04d875e0f
chrisconlon <christopherconlon@gmail.com>
2020-06-25 12:04:02 -0400

    Update 2_Process_WRDS_Data.py
    stray space.

--- code/2_Process_WRDS_Data.py
+++ code/2_Process_WRDS_Data.py
@@ -98,7 +98,7 @@ dfs.to_parquet(f_betas_scraped,compression='brotli')
 # - Save the combined FrankenBeta file
 
 # use TR before cut-date and scraped data after
-df=combine_betas(df1,dfs,cut_dat e='2000-01-01')
+df=combine_betas(df1,dfs,cut_date='2000-01-01')
 df.to_parquet(f_frankenbetas,compression='brotli')
 
 ### Checks
